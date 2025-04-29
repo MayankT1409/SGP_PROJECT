5828bcdac9515ad57c65162eb44f31f9f2b02e5d
MayankT1409 <23it135@charusat.edu.in>
2025-04-30 05:12:10 +0530

    updates

--- Backend/index.js
+++ Backend/index.js
@@ -9,11 +9,22 @@ dotenv.config();
 
 const app = express();
 
+
 // CORS config
+const allowedOrigins = [
+  'http://localhost:5173',                  // development
+  'https://sgp-project-1.onrender.com'      // production
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,  // 'https://heritage-sites.onrender.com'
-  credentials: true,
+  origin: allowedOrigins,
+  credentials: true
 }));
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//   credentials: true,
+// }));
 
 app.use(express.json());
 
